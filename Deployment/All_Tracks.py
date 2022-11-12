0ebf484f5fab3d15b3f7eb507e806807a20b9d09
Samyuktha Patnaik <92504503+SamyukthaPatnaik@users.noreply.github.com>
2022-11-12 23:40:22 +0530

    Update All_Tracks.py

--- Deployment/All_Tracks.py
+++ Deployment/All_Tracks.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 
+#Loading all the tracks  
 music = pickle.load(open('music.pkl', 'rb'))
 musicdf = pd.DataFrame(music)
 st.title("All Tracks ")
