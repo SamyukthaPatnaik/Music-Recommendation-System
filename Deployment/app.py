6e035ab7831e37293d9db750ca56d3cb21faca0c
Samyuktha Patnaik <92504503+SamyukthaPatnaik@users.noreply.github.com>
2022-11-21 23:53:59 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -12,4 +12,4 @@ st.write("3. It will Recommend you Top 5 Music Tracks by-default or else you can
 st.write("4. Click on URL of any Music Tracks, it will redirect you to preview of that song. ")
 st.write("5. If you like that particular song then we also gave spotify link so that you can copy past it to the web browser.")
 st.write("6. You can also have a look on (All Tracks) in the sidebar.")
-st.success("Enjoy the Song of Your Choice.")
+st.success("Enjoy the Music of Your Choice.")
